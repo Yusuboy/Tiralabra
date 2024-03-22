9a026d41af30d23015448ea28b67d58ab53f93ca
Yusuboy <Yuusufab@outlook.com>
2024-03-22 04:04:16 +0200

    LZW pakkaus valmis

---
+++ src/LZW/compress.py
@@ -0,0 +1,32 @@
+def compress(input_file, output_file):
+    lzw_dictionary = {chr(i): i for i in range(256)}
+    next_code = 256
+
+    with open(input_file, 'r') as file_open:
+        data = file_open.read()
+
+    compressed_section = []
+    sequence = ""
+    for char in data:
+        if sequence + char in lzw_dictionary:
+            sequence = sequence + char
+        else:
+            compressed_section.append(lzw_dictionary[sequence])
+            lzw_dictionary[sequence + char] = next_code
+            next_code += 1
+            sequence = char
+
+    if sequence in lzw_dictionary:
+        compressed_section.append(lzw_dictionary[sequence])
+
+    # print("compressed data:", compressed_section)
+    
+    with open(output_file, 'wb') as output:
+        for char in compressed_section:
+            output.write(char.to_bytes(2, "big"))
+        
+    print("Compressed data saved to", output_file)
+
+chosen_file = "example.txt"
+output_file = "compressed_example.bin"
+compress(chosen_file, output_file)
