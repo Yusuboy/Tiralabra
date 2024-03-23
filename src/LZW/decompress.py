95be6a8f86d50483f1eff26a0b27ca012e5d1f46
Yusuboy <Yuusufab@outlook.com>
2024-03-23 14:10:19 +0200

    Dockstring added

--- src/LZW/decompress.py
+++ src/LZW/decompress.py
@@ -1,11 +1,31 @@
 class LZWDecompressor:
+    """
+    Class for decompressing data using LZW decomresss algorithm
+
+    Attributes:
+        None
+    """
     def decompress(self, input_file, output_file):
+        """Decompresses data from a input file and saves the decompressed data to the output file.
+
+        Args:
+            input_file (str): The path to the file containing compressed data.
+            output_file (str): The path to the output file where the decompressed data will be saved.
+
+        Return:
+            None
+
+        Raises:
+            FileNotFoundError: If the input file does not exist.
+        """
         lzw_dictionary = dict([(x, chr(x)) for x in range(256)])
         next_code = 256
 
+        try:
+            compressed_data = open(input_file, "rb")
+        except FileNotFoundError:
+            raise FileNotFoundError("Input file not found.")
         
-        compressed_data = open(input_file, "rb")
-
         compressed_section = []
         decompressed_section = ""
         sequence = ""
@@ -13,20 +33,14 @@ class LZWDecompressor:
         while True:
             # Read two bytes from the file
             bytes_read = compressed_data.read(2)
-
             # Check if both bytes were successfully read
             if len(bytes_read) != 2:
                 # If either byte is missing, it means we've reached the end of the file
                 break
-
             # Convert the bytes to a 16-bit integer
             data = int.from_bytes(bytes_read, byteorder='big')
-
             # Append the integer to the list
             compressed_section.append(data)
-
-
-
 
         for byte in compressed_section:
             # sequences += chr(byte)
