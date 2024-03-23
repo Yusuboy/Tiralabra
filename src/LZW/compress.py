50901ad5ac3c5e494f040c7482aa5d27cb53f1bc
Yusuboy <Yuusufab@outlook.com>
2024-03-23 02:06:58 +0200

    Defined classes and modified main

--- src/LZW/compress.py
+++ src/LZW/compress.py
@@ -1,32 +1,29 @@
-def compress(input_file, output_file):
-    lzw_dictionary = {chr(i): i for i in range(256)}
-    next_code = 256
+class LZWCompressor:
+    def compress(self, input_file, output_file):
+        lzw_dictionary = {chr(i): i for i in range(256)}
+        next_code = 256
 
-    with open(input_file, 'r') as file_open:
-        data = file_open.read()
+        with open(input_file, 'r', encoding='ASCII') as file_open:
+            data = file_open.read()
 
-    compressed_section = []
-    sequence = ""
-    for char in data:
-        if sequence + char in lzw_dictionary:
-            sequence = sequence + char
-        else:
+        compressed_section = []
+        sequence = ""
+        for char in data:
+            if sequence + char in lzw_dictionary:
+                sequence = sequence + char
+            else:
+                compressed_section.append(lzw_dictionary[sequence])
+                lzw_dictionary[sequence + char] = next_code
+                next_code += 1
+                sequence = char
+
+        if sequence in lzw_dictionary:
             compressed_section.append(lzw_dictionary[sequence])
-            lzw_dictionary[sequence + char] = next_code
-            next_code += 1
-            sequence = char
 
-    if sequence in lzw_dictionary:
-        compressed_section.append(lzw_dictionary[sequence])
+        print("compressed data:", compressed_section)
 
-    # print("compressed data:", compressed_section)
-    
-    with open(output_file, 'wb') as output:
-        for char in compressed_section:
-            output.write(char.to_bytes(2, "big"))
-        
-    print("Compressed data saved to", output_file)
+        with open(output_file, 'wb') as output:
+            for char in compressed_section:
+                output.write(char.to_bytes(2, "big"))
 
-chosen_file = "example.txt"
-output_file = "compressed_example.bin"
-compress(chosen_file, output_file)
+        print("Compressed data saved to", output_file)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print ( "Hello world")
+from LZW.compress import LZWCompressor
+
+CHOSEN_FILE = "src/example.txt"
+OUTPUT_FILE = "src/compressed_example.bin"
+
+compressor = LZWCompressor()
+compressor.compress(CHOSEN_FILE, OUTPUT_FILE)
