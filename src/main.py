0fc743c3a6bb7d22e917e39c1f714d25afa83206
Yusuboy <Yuusufab@outlook.com>
2024-04-06 20:52:51 +0300

    updated main.py

--- src/main.py
+++ src/main.py
@@ -5,21 +5,26 @@ from HuffmanCoding.compress import HuffmanCompress
 
 
 CHOSEN_FILE = "src/example.txt"
-# OUTPUT_FILE = "src/compressed_example.bin"
 
-# compressor = LZWCompressor()
-# decompressor = LZWDecompressor()
-# compressor.compress(CHOSEN_FILE, OUTPUT_FILE)
-
-DECOMPRESSED_FILE = "src/decompressed.txt"
-
-# decompressor.decompress(OUTPUT_FILE, DECOMPRESSED_FILE)
+OUTPUT_FILE = "src/compressed_W_Huff_example.bin"
+OUTPUT_FILE2 = "src/compressed_W_LZW_example.bin"
 
 
-OUTPUT_FILE = "src/compressed_example.bin"
+DECOMPRESSED_FILE = "src/decompressed_W_Huff.txt"
+DECOMPRESSED_FILE2 = "src/decompressed_W_LZW.txt"
 
-c = HuffmanCompress()
 
-c.compress(CHOSEN_FILE, OUTPUT_FILE)
-c.decompress(OUTPUT_FILE, DECOMPRESSED_FILE)
 
+
+
+
+Huffman = HuffmanCompress()
+
+Huffman.compress(CHOSEN_FILE, OUTPUT_FILE)
+Huffman.decompress(OUTPUT_FILE, DECOMPRESSED_FILE)
+
+compressor = LZWCompressor()
+decompressor = LZWDecompressor()
+
+compressor.compress(CHOSEN_FILE, OUTPUT_FILE2)
+decompressor.decompress(OUTPUT_FILE2, DECOMPRESSED_FILE2)
